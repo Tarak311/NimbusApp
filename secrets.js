c15b9609291e521e79d9ff9e3047ff38ff3615b9
Tarak311 <tarak_1@live.com>
2021-01-23 01:41:33 -0500

    new

---
+++ secrets.js
@@ -0,0 +1,6 @@
+var mongourl =  "mongodb+srv://usera:userb@littlemongocluster.ge3qg.mongodb.net/nimbusapp?retryWrites=true&w=majority"
+var secretpswd = "notsoobivios"
+export default {
+    url : mongourl ,
+    pswd : secretpswd,
+}
