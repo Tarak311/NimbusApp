df728d3044545e629dbd3ed11d93128cd6d3e727
Tarak311 <tarak_1@live.com>
2021-01-23 01:56:59 -0500

    dependent on env

--- secrets.js
+++ secrets.js
@@ -1,5 +1,7 @@
-var mongourl =  "mongodb+srv://usera:userb@littlemongocluster.ge3qg.mongodb.net/nimbusapp?retryWrites=true&w=majority"
-var secretpswd = "notsoobivios"
+const env = process.env;
+
+var mongourl = process.env.ALTURL ||  "mongodb+srv://usera:userb@littlemongocluster.ge3qg.mongodb.net/nimbusapp?retryWrites=true&w=majority"
+var secretpswd = process.env.PAWD ||"notsoobivios"
 export default {
     url : mongourl ,
     pswd : secretpswd,
